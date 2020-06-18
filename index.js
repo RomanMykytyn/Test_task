c3e5f62d1874ffa676554661ae8e711513635402
Roman Mykyryn <roman.mykytyn89@gmail.com>
2020-06-18 16:33:25 +0300

    Heroku4

--- index.js
+++ index.js
@@ -71,6 +71,7 @@ app.use('/login', loginRouter);
 app.use('/user', userRouter);
 app.use('/friend', friendRouter);
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!!!');
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
